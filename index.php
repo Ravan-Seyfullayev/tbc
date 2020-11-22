c223e91915872351dadd9459c4d242a6d891abad
Ravan-Seyfullayev <68846252+Ravan-Seyfullayev@users.noreply.github.com>
2020-11-22 15:18:44 +0400

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Məktəb</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="signIn">
+        <h2>Daxil ol</h2>
+        <label for="name">Ad</label>
+        <input type="text">
+        <label for="password">Parol</label>
+        <input type="password">
+    </div>
+</body>
+</html>
